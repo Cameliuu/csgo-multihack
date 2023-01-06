c0cbdd6a561a4a902eadcdde568b143186d1def9
Cameliuu <kozo8951@gmail.com>
2023-01-05 16:45:11 -0800

    Add offsets class

---
+++ multihack/Offsets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multihack
+{
+    public class Offsets
+    {
+        public Int32 localPlayer = 0xDEA964;
+        public Int32 entityList = 0x4DFFF14;
+        public Int32 crosshairId = 0x11838;
+        public Int32 team = 0xF4;
+        public Int32 health = 0x100;
+        public Int32 forceAttack = 0x322DD10;
+    }
+}
